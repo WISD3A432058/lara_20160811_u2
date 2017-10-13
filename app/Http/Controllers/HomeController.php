68f228f001c72e2b242180c0e4850acdd55999ba
M511-52\admin <s3A432058@students.ncut.edu.tw>
2017-10-13 16:37:24 +0800

    設定回傳預設首頁

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -4,11 +4,13 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+//設定回傳預設首頁
 class HomeController extends Controller
 {
-    publicfunctionindex()
+    public function index()
     {
-return'index';
+returnview('welcome');
 }
 }
+
 
